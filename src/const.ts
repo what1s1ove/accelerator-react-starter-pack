761a7a9afdc3f9667bc2353d6ec22d1862325038
Tatiaynti <tatianamo1234@mail.ru>
2021-12-20 21:52:39 +0300

    adds sorting

--- src/const.ts
+++ src/const.ts
@@ -7,6 +7,17 @@ enum AppRoute {
 const APIRoute  = {
   Catalog: '/guitars',
   Comments: (id: number): string => `/guitars/${id}/comments`,
+  Sort: (sort: string, order: string): string => `/guitars?_sort=${sort}&_order=${order}`,
 };
 
-export {AppRoute, APIRoute};
+enum SortType {
+  Price = 'price',
+  Rating = 'rating',
+}
+
+enum SortOrder {
+  Asc = 'asc',
+  Desc = 'desc',
+}
+
+export {AppRoute, APIRoute, SortType, SortOrder};
