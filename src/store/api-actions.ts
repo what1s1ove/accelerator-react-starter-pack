60cc7ee88e3202d730deae493f62fbcba12b60c4
Ivan <work.gorobec@gmail.com>
2021-12-26 01:00:10 +0500

    добавит слово Action в название функции

--- src/store/api-actions.ts
+++ src/store/api-actions.ts
@@ -1,10 +1,10 @@
 import {ThunkActionResult} from '../types/action';
 import {APIRoute} from '../const/const';
 import {GuitarsList} from '../types/guitar';
-import {fillGuitarsList} from './action';
+import {fillGuitarsListAction} from './action';
 
 export const fetchGuitarsList = ():ThunkActionResult =>
   async (dispatch, _getState, api): Promise<void> => {
     const {data} = await api.get<GuitarsList>(APIRoute.Guitars);
-    dispatch(fillGuitarsList(data));
+    dispatch(fillGuitarsListAction(data));
   };
