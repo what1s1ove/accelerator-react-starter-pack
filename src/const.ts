153aaac6618b74f5b411ab61f0b22adf2d266d30
Tatiaynti <tatianamo1234@mail.ru>
2021-12-25 19:17:25 +0300

    adds utils, combines filters in one component

--- src/const.ts
+++ src/const.ts
@@ -2,22 +2,62 @@ enum AppRoute {
   Catalog = '/',
   Guitar = '/guitars/:id',
   PageNotFound = '*',
+  Filter = '/guitars',
 }
 
 const APIRoute  = {
   Catalog: '/guitars',
   Comments: (id: number): string => `/guitars/${id}/comments`,
-  Sort: (sort: string, order: string): string => `/guitars?_sort=${sort}&_order=${order}`,
+  Sort: ((filterQuery: string, sortType: string, sortOrder?: string): string => `/guitars${filterQuery ? `${filterQuery}&` : '?'}${sortType}${sortOrder ? sortOrder : ''}`),
+  Filter: ((query: string): string => `/guitars${query}`),
 };
 
 enum SortType {
-  Price = 'price',
-  Rating = 'rating',
+  Price = '_sort=price',
+  Rating = '_sort=rating',
 }
 
 enum SortOrder {
-  Asc = 'asc',
-  Desc = 'desc',
+  Asc = '&_order=asc',
+  Desc = '&_order=desc',
 }
 
-export {AppRoute, APIRoute, SortType, SortOrder};
+enum FilterParams {
+  MinPrice = 'price_gte=',
+  MaxPrice = 'price_lte=',
+  Type = 'type=',
+  FilterStringCount = 'stringCount=',
+}
+
+const stringValues = [4, 6, 7, 12];
+
+enum FilterByType {
+  Acoustic = 'acoustic',
+  Electric = 'electric',
+  Ukulele = 'ukulele',
+}
+
+const filtersByStringAndType = [
+  {
+    elementId: '4-strings',
+    value: 4,
+    guitarTypes: [FilterByType.Electric.toString(), FilterByType.Ukulele.toString()],
+  },
+  {
+    elementId: '6-strings',
+    value: 6,
+    guitarTypes: [FilterByType.Acoustic.toString(), FilterByType.Electric.toString()],
+  },
+  {
+    elementId: '7-strings',
+    value: 7,
+    guitarTypes: [FilterByType.Acoustic.toString(), FilterByType.Electric.toString()],
+  },
+  {
+    elementId: '12-strings',
+    value: 12,
+    guitarTypes: [FilterByType.Acoustic.toString()],
+  },
+];
+
+export {AppRoute, APIRoute, SortType, SortOrder, FilterParams, stringValues, filtersByStringAndType};
