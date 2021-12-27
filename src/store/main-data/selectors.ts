24f54b8a8b80c90e29fa62998e39f69519466fb1
Ivan <work.gorobec@gmail.com>
2021-12-28 04:14:11 +0500

    удалит файлы redux

--- src/store/main-data/selectors.ts
+++
@@ -1,5 +0,0 @@
-import {State} from '../../types/state';
-import {GuitarsList} from '../../types/guitar';
-import {NameSpace} from '../root-reducer';
-
-export const getGuitarsList = (state:State): GuitarsList => state[NameSpace.main].guitarsList;
--- src/types/state.ts
+++
@@ -1,9 +0,0 @@
-import {GuitarsList} from './guitar';
-import {RootState} from '../store/root-reducer';
-
-export type MainData = {
-  guitarsList: GuitarsList,
-  isDataLoaded: boolean,
-};
-
-export type State = RootState;
