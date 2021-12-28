aa2f98e35d8ff1a8aeccd082f0b7566fd9bab890
Tatiaynti <tatianamo1234@mail.ru>
2021-12-28 13:10:19 +0300

    refactores links

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+import { AppRoute } from '../../const';
 import HeaderFormSearch from './header-form-search';
 
 function Header(): JSX.Element {
@@ -6,20 +8,20 @@ function Header(): JSX.Element {
       <div className="container header__wrapper"><a className="header__logo logo" href="main.html"> <img className="logo__img" width="70" height="70" src="./img/svg/logo.svg" alt="Логотип" /></a>
         <nav className="main-nav">
           <ul className="main-nav__list">
-            <li><a className="link main-nav__link" href="#">Каталог</a>
+            <li><Link className="link main-nav__link" to={AppRoute.Catalog}>Каталог</Link>
             </li>
-            <li><a className="link main-nav__link" href="#">Где купить?</a>
+            <li><Link className="link main-nav__link" href="#" to={AppRoute.PageNotFound}>Где купить?</Link>
             </li>
-            <li><a className="link main-nav__link" href="#">О компании</a>
+            <li><Link className="link main-nav__link" href="#" to={AppRoute.PageNotFound}>О компании</Link>
             </li>
           </ul>
         </nav>
         <HeaderFormSearch />
-        <a className="header__cart-link" href="#" aria-label="Корзина">
+        <Link className="header__cart-link" aria-label="Корзина" to={AppRoute.PageNotFound}>
           <svg className="header__cart-icon" width="14" height="14" aria-hidden="true">
             <use xlinkHref="#icon-basket"></use>
           </svg><span className="visually-hidden">Перейти в корзину</span><span className="header__cart-count">2</span>
-        </a>
+        </Link>
       </div>
     </header>
   );
