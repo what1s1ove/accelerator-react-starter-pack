783408204afbb10cb89789268ae1345073bdec1a
Ivan <work.gorobec@gmail.com>
2022-01-05 03:45:13 +0500

    подкорректирует query основного запроса

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,35 +1,66 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/dist/query/react';
-import {GuitarsList} from '../types/guitar';
+import {Guitar, GuitarsList} from '../types/guitar';
 
 const BACKEND_URL = 'https://accelerator-guitar-shop-api-v1.glitch.me/';
+
+const getURL = (type:string | undefined, stringCount:string | undefined):string => {
+  if (type && stringCount === undefined) {
+    return `/guitars?type=${type}`;
+  }
+  else if (stringCount && type === undefined) {
+    return `/guitars?stringCount=${stringCount}`;
+  }
+  else if (type && stringCount) {
+    return `/guitars?type=${type}&stringCount=${stringCount}`;
+  }
+  else {
+    return '/guitars';
+  }
+};
 
 export const mainAPI = createApi({
   reducerPath: 'mainAPI',
   baseQuery: fetchBaseQuery({baseUrl: BACKEND_URL}),
   endpoints: (build) => ({
-    fetchGuitarsList: build.query<GuitarsList, { limit: number; sort?: string; order?:string }> ( {
-      query: ({
-        limit = 9,
-        sort = 'price',
-        order = 'asc',
-      }) => ({
+    fetchGuitarsList: build.query<GuitarsList,
+      { limit: number; sort:string | undefined; order:string | undefined; type:string | undefined, stringCount:string | undefined; } > ( {
+        query: ({limit, sort, order, type, stringCount}) => ({
+          url: getURL(type, stringCount),
+          params: {
+            _limit: limit,
+            _sort: sort,
+            _order: order,
+            stringCount: stringCount,
+          },
+        }),
+      }),
+    fetchAlikeGuitars: build.query<GuitarsList, string> ( {
+      query: (name?:string ) => ({
+        url: `/guitars?name_like=${name}`,
+      }),
+    }),
+    fetchMinPrice: build.query<Guitar[], ''> ( {
+      query: () => ({
         url: '/guitars',
         params: {
-          _limit: limit,
-          _sort: sort,
-          _order: order,
+          _limit: 1, _sort: 'price', _order: 'asc',
         },
       }),
     }),
-    fetchAlikeGuitars: build.query<GuitarsList, string> ( {
-      query: (name:string) => ({
+    fetchMaxPrice: build.query<Guitar[], ''> ( {
+      query: () => ({
         url: '/guitars',
         params: {
-          'name_like': name,
+          _limit: 1, _sort: 'price', _order: 'desc',
         },
       }),
     }),
   }),
 });
 
-export const {useFetchGuitarsListQuery, useFetchAlikeGuitarsQuery} = mainAPI;
+export const {
+  useFetchGuitarsListQuery,
+  useFetchAlikeGuitarsQuery,
+  useFetchMinPriceQuery,
+  useFetchMaxPriceQuery,
+} = mainAPI;
