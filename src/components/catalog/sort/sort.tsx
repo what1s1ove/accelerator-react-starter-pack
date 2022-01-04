4a39f5decbcd9f2b3d16561939a6746bb1b75a9c
Ivan <work.gorobec@gmail.com>
2022-01-05 03:47:34 +0500

    добавит корректировку url при воздействии на сортировку

--- src/components/catalog/sort/sort.tsx
+++ src/components/catalog/sort/sort.tsx
@@ -1,26 +1,58 @@
 import React, {MouseEvent, useState} from 'react';
-import {OrderOfSort, SortType, TypeOfSort} from '../../../const/const';
+import {SortByOrder, SortType, SortByType} from '../../../const/const';
+import _ from 'lodash';
 
 type SortProps = {
-  onSortChange: (sort: string | undefined) => void;
-  onOrderChange: (order: string | undefined) => void;
+  onStateSortChange: (sort: string | undefined) => void;
+  onStateOrderChange: (order: string | undefined) => void;
+  viewState: {[p: string]: string};
+  changeURL: (updatedViewState: {[p: string]: string}) => void;
 }
 
 const ACTIVE = -1;
 const NOT_ACTIVE = 0;
 
-function Sort({onSortChange, onOrderChange}:SortProps):JSX.Element {
-  const [sortType, setSortType] = useState('');
-  const [sortOrder, setSortOrder] = useState('');
+const getSort = (sort:string):string => SortByType.get(sort) as string;
+const getOrder = (order:string): string => SortByOrder.get(order) as string;
+
+function Sort({onStateSortChange, onStateOrderChange, viewState, changeURL}:SortProps):JSX.Element {
+  const [currentSortType, setCurrentSortType] = useState('');
+  const [currentSortOrder, setCurrentSortOrder] = useState('');
 
   const handleSortClick = (sort: string) => {
-    onSortChange(TypeOfSort.get(sort));
-    setSortType(sort);
+    currentSortOrder.length === 0
+      ? changeURL({...viewState, 'sort': getSort(sort), 'order': getOrder(SortType.Ascend)})
+      : changeURL({...viewState, 'sort': getSort(sort)});
+    onStateSortChange(getSort(sort));
+    setCurrentSortType(sort);
+    if (currentSortOrder.length === 0) {
+      onStateOrderChange(getOrder(SortType.Ascend));
+      setCurrentSortOrder(SortType.Ascend);
+    }
+    if (sort === currentSortType) {
+      onStateSortChange('');
+      setCurrentSortType('');
+      const newViewState = _.omit(viewState, 'sort');
+      changeURL(newViewState);
+    }
   };
 
   const handleOrderClick = (order:string) => {
-    onOrderChange(OrderOfSort.get(order));
-    setSortOrder(order);
+    currentSortType.length === 0
+      ? changeURL({...viewState, 'order': getOrder(order),  'sort': getSort(SortType.Price)})
+      : changeURL({...viewState, 'order': getOrder(order)});
+    onStateOrderChange(SortByOrder.get(order));
+    setCurrentSortOrder(order);
+    if (currentSortType.length === 0) {
+      onStateSortChange(getSort(SortType.Price));
+      setCurrentSortType(SortType.Price);
+    }
+    if (order === currentSortOrder) {
+      onStateOrderChange('');
+      setCurrentSortOrder('');
+      const newViewState = _.omit(viewState, 'order');
+      changeURL(newViewState);
+    }
   };
 
   return (
@@ -28,18 +60,18 @@ function Sort({onSortChange, onOrderChange}:SortProps):JSX.Element {
       <h2 className="catalog-sort__title">Сортировать:</h2>
       <div className="catalog-sort__type">
         <button
-          className={`catalog-sort__type-button ${sortType === SortType.Price ? 'catalog-sort__type-button--active': ''}`}
+          className={`catalog-sort__type-button ${currentSortType === SortType.Price ? 'catalog-sort__type-button--active': ''}`}
           aria-label="по цене"
-          tabIndex={sortType === SortType.Price ? ACTIVE : NOT_ACTIVE}
+          tabIndex={currentSortType === SortType.Price ? ACTIVE : NOT_ACTIVE}
           onClick={({currentTarget}:MouseEvent<HTMLButtonElement>) => {
             handleSortClick(currentTarget.ariaLabel);
           }}
         >по цене
         </button>
         <button
-          className={`catalog-sort__type-button ${sortType === SortType.Popularity ? 'catalog-sort__type-button--active': ''}`}
+          className={`catalog-sort__type-button ${currentSortType === SortType.Popularity ? 'catalog-sort__type-button--active': ''}`}
           aria-label="по популярности"
-          tabIndex={sortType === SortType.Popularity ? ACTIVE : NOT_ACTIVE}
+          tabIndex={currentSortType === SortType.Popularity ? ACTIVE : NOT_ACTIVE}
           onClick={({currentTarget}:MouseEvent<HTMLButtonElement>) => {
             handleSortClick(currentTarget.ariaLabel);
           }}
@@ -48,18 +80,18 @@ function Sort({onSortChange, onOrderChange}:SortProps):JSX.Element {
       </div>
       <div className="catalog-sort__order">
         <button
-          className={`catalog-sort__order-button catalog-sort__order-button--up ${sortOrder === SortType.Ascend ? 'catalog-sort__order-button--active' : ''}`}
+          className={`catalog-sort__order-button catalog-sort__order-button--up ${currentSortOrder === SortType.Ascend ? 'catalog-sort__order-button--active' : ''}`}
           aria-label="По возрастанию"
-          tabIndex={sortOrder === SortType.Ascend ? ACTIVE : NOT_ACTIVE}
+          tabIndex={currentSortOrder === SortType.Ascend ? ACTIVE : NOT_ACTIVE}
           onClick={({currentTarget}:MouseEvent<HTMLButtonElement>) => {
             handleOrderClick(currentTarget.ariaLabel);
           }}
         >
         </button>
         <button
-          className={`catalog-sort__order-button catalog-sort__order-button--down ${sortOrder === SortType.Descend ? 'catalog-sort__order-button--active' : ''}`}
+          className={`catalog-sort__order-button catalog-sort__order-button--down ${currentSortOrder === SortType.Descend ? 'catalog-sort__order-button--active' : ''}`}
           aria-label="По убыванию"
-          tabIndex={sortOrder === SortType.Descend ? ACTIVE : NOT_ACTIVE}
+          tabIndex={currentSortOrder === SortType.Descend ? ACTIVE : NOT_ACTIVE}
           onClick={({currentTarget}:MouseEvent<HTMLButtonElement>) => {
             handleOrderClick(currentTarget.ariaLabel);
           }}
