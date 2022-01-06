37a49d2df876c0c48948d0c1c1aba60316f230d7
Ivan <work.gorobec@gmail.com>
2022-01-06 22:49:41 +0500

    уберет лишние пробелы

--- src/components/catalog/sort/searchResult.tsx
+++ src/components/catalog/sort/searchResult.tsx
@@ -11,10 +11,11 @@ function SearchResult ({searchTerm}:SearchTermProps) {
   const {data} = useFetchAlikeGuitarsQuery(name);
 
   useEffect(() => {
-    if ( searchTerm && (searchTerm.length === 0 || searchTerm.length > 2)) {
+    if (searchTerm && (searchTerm.length === 0 || searchTerm.length > 1)) {
       setName(searchTerm);
     }
   }, [searchTerm]);
+
 
   return (
     <ul style={{zIndex: 1}} className={`form-search__select-list ${!name ? 'hidden' : ''}`}>
