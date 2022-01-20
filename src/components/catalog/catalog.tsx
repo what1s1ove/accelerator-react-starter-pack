f9befeac1f962c861f6950767ca79be8a2bb3319
Tatiaynti <tatianamo1234@mail.ru>
2022-01-20 22:25:07 +0300

    Update catalog.tsx

--- src/components/catalog/catalog.tsx
+++ src/components/catalog/catalog.tsx
@@ -1,9 +1,9 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { FetchGuitarProperty, FetchStatus, QueryParam } from '../../const';
+import { FetchGuitarProperty, QueryParam } from '../../const';
 import { useQueryParams } from '../../hooks/use-query-params';
-import { fetchFilteredGuitarsAction, fetchGuitarsOnPageAction } from '../../store/api-actions';
-import { getCatalogFetchStatusSelector, getGuitars } from '../../store/selectors';
+import { fetchGuitarsAction, fetchGuitarsOnPageAction } from '../../store/api-actions';
+import { getDataLoadingStatus, getGuitars } from '../../store/selectors';
 import CatalogFilters from '../catalog-filters/catalog-filters';
 import CatalogPagination from '../catalog-pagination/catalog-pagination';
 import CatalogSort from '../catalog-sort/catalog-sort';
@@ -12,6 +12,7 @@ import ProductCards from '../product-cards/product-cards';
 function Catalog(): JSX.Element {
   const dispatch = useDispatch();
   const queryParams = useQueryParams();
+  const isDataLoaded = useSelector(getDataLoadingStatus);
 
   const querySortType = queryParams.has(QueryParam.Sort) ? queryParams.get(QueryParam.Sort) : '';
   const queryOrderType = queryParams.has(QueryParam.Order) ? queryParams.get(QueryParam.Order) : '';
@@ -42,26 +43,26 @@ function Catalog(): JSX.Element {
       currentPageNumber: queryCurrentPage,
     };
     dispatch(fetchGuitarsOnPageAction(fetchParams));
-    dispatch(fetchFilteredGuitarsAction(fetchParams));
+    dispatch(fetchGuitarsAction(fetchParams));
   }, [dispatch, queryAcusticType, queryCurrentPage, queryElectricType, queryFourString, queryOrderType, querySevenString, querySixString, querySortType, queryTwelveString, queryUkuleleType, queryUserPriceMax, queryUserPriceMin]);
 
   const catalogCards = useSelector(getGuitars);
-  const fetchStatus = useSelector(getCatalogFetchStatusSelector);
+
+  if (!isDataLoaded) {
+    return (
+      <div className="catalog">
+        <CatalogFilters />
+        <CatalogSort />
+        Loading...
+      </div>
+    );
+  }
 
   return (
-    <div className="catalog" data-testid='catalog'>
+    <div className="catalog">
       <CatalogFilters />
       <CatalogSort />
-      {
-        fetchStatus === FetchStatus.InProgress
-        && <>Loading...</>
-      }
-      {
-        fetchStatus === FetchStatus.Success
-        && catalogCards.length > 0
-        &&
-        <ProductCards productCards={catalogCards} />
-      }
+      <ProductCards productCards={catalogCards} />
       <CatalogPagination />
     </div>
   );
