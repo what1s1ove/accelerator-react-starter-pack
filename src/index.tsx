1e850ccdb3189133a53094622e780ef99b8ac9e5
Tatiaynti <tatianamo1234@mail.ru>
2022-01-20 22:01:41 +0300

    updates index

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 import App from './components/app/app';
 import { createAPI } from './services/api';
-// import { fetchGuitarsAction } from './store/api-actions';
 import { redirect } from './store/middlewares/redirect';
 import { rootReducer } from './store/root-reducer';
 import { Router as BrowserRouter } from 'react-router-dom';
@@ -20,8 +19,6 @@ const store = configureStore({
       },
     }).concat(redirect),
 });
-
-// store.dispatch(fetchGuitarsAction());
 
 ReactDOM.render(
   <React.StrictMode>
