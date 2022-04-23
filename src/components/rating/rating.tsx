d08d10b829e32fcb3445ca83924789716482d1f1
Andrei Druzhaev <andrewdruzhaev@gmail.com>
2022-04-23 20:08:38 +0300

    Update Rating component to count rating from api

--- src/components/rating/rating.tsx
+++ src/components/rating/rating.tsx
@@ -1,19 +1,28 @@
-/* eslint-disable react/jsx-no-useless-fragment */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Icon } from '../icon/icon';
+import { nanoid } from 'nanoid';
 import sprite from '../../assets/sprite.svg';
 
-export function Rating() {
-  const [ratingArray, setRatingArray] = useState<JSX.Element[]>(new Array(5).fill(<></>));
-  //   const [ratingArray, setRatingArray] = useState(new Array(5));
+export function Rating(props: {
+  rating: number
+}) {
+  const rating = Math.ceil(props.rating);
+  const maxRating = 5;
+  const [ratingArray, setRatingArray] = useState<JSX.Element[]>(new Array(rating).fill(<Icon name={`${sprite}#full-star`} color="#C90606" width="14" height="12" />));
+
+  const updateRating = (currentRating: number) => {
+    if (currentRating < maxRating) {
+      setRatingArray((oldArray) => [...oldArray, ...(new Array(Math.ceil(maxRating - currentRating)).fill(<Icon name={`${sprite}#empty-star`} color="none" width="14" height="12" />))]);
+    }
+  };
+
+  useEffect(() => {
+    updateRating(rating);
+  }, [rating]);
 
   return (
     <div>
-      {ratingArray.map((item) => (
-        <span key={item.key}>
-          <Icon name={`${sprite}#full-star`} color="#C90606" width="14" height="12" />
-        </span>))}
+      {ratingArray.map((item) => <span key={nanoid()}>{item}</span>)}
     </div>
   );
 }
