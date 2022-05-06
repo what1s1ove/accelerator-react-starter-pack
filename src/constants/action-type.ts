6626b251f14a5ba5d834d64765a9d0be88281450
Andrei Druzhaev <andrewdruzhaev@gmail.com>
2022-05-06 08:28:17 +0300

    Use createAsyncThunk for api calls

--- src/constants/action-type.ts
+++ src/constants/action-type.ts
@@ -1,14 +1,5 @@
 export const ActionType = {
-  LOAD_GUITARS: 'guitars/load',
-  LOAD_GUITARS_BY_NAME: 'guitars/loadByName',
-  LOAD_FILTERED_GUITARS: 'guitars/loadFiltered ',
-  LOAD_SORTING_TYPE: 'sorting/loadType',
-  LOAD_SORTING_ORDER: 'sorting/loadOrder',
-  LOAD_QUANTITY_OF_STRINGS: 'filters/loadQuantityOfStrings',
-  REMOVE_QUANTITY_OF_STRINGS: 'filters/removeQuantityOfStrings',
-  LOAD_GUITAR_TYPE: 'filters/loadGuitarType',
-  REMOVE_GUITAR_TYPE: 'filters/removeGuitarType',
-  LOAD_PRICE_RANGE: 'filters/loadPriceRange',
-  LOAD_CURRENT_PAGE: 'pagination/loadCurrentPage',
-  LOAD_ALL_PAGES: 'pagination/loadAllPages',
+  FETCH_GUITARS: 'guitars/fetchGuitarsList',
+  FETCH_FILTERED_GUITARS: 'guitars/fetchFilteredGuitarsList',
+  FETCH_GUITARS_BY_NAME: 'guitars/fetchGuitarsListByName',
 } as const;
--- src/store/guitars/api-actions.ts
+++ src/store/guitars/api-actions.ts
@@ -1,47 +1,58 @@
 import { loadFilteredGuitars, loadGuitars, loadGuitarsByName } from './slice';
 import { ApiRoute } from '../../constants/api-route';
-import { RootState } from '../store';
-import { ThunkAction } from '@reduxjs/toolkit';
-import { Action } from 'redux';
-import { AxiosInstance } from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
 import { QueryParametersType } from '../../types/query-params';
 import { QueryParam } from '../../constants/query-param';
 import { loadTotalPageCount } from '../pagination/slice';
+import { axiosInstance } from '../../api/api';
+import { IFilters } from '../../types/IFilters';
+import { IPagination } from '../../types/IPagination';
+import { ActionType } from '../../constants/action-type';
 
 const GUITARS_PER_PAGE = 9;
 const TotalCountHeader = 'x-total-count';
 const EmbedComments = 'comments';
 
-export const fetchGuitarsList = (): ThunkAction<Promise<void>, RootState, AxiosInstance, Action> => async (dispatch, _getState, api) => {
-  const response = await api.get(`${process.env.REACT_APP_SERVER_URL}${ApiRoute.Guitars}`);
-  dispatch(loadGuitars(response.data));
-};
+export const fetchGuitarsList = createAsyncThunk(
+  ActionType.FETCH_GUITARS,
+  async (_, thunkApi) => {
+    const response = await axiosInstance.get(`${process.env.REACT_APP_SERVER_URL}${ApiRoute.Guitars}`);
+    thunkApi.dispatch(loadGuitars(response.data));
+  },
+);
 
-export const fetchFilteredGuitarsList = (params?: QueryParametersType): ThunkAction<Promise<void>, RootState, AxiosInstance, Action> => async (dispatch, _getState, api) => {
-  const response = await api.get(`${process.env.REACT_APP_SERVER_URL}${ApiRoute.Guitars}`, {
-    params: {
-      [QueryParam.StringCount]: _getState().filters.quantityOfStrings,
-      [QueryParam.Type]: _getState().filters.guitarType,
-      [QueryParam.PriceGte]: _getState().filters.priceRange.min || null,
-      [QueryParam.PriceLte]: _getState().filters.priceRange.max || null,
-      [QueryParam.Limit]: GUITARS_PER_PAGE,
-      [QueryParam.Start]: GUITARS_PER_PAGE * (_getState().pagination.currentPage - 1),
-      [QueryParam.Embed]: EmbedComments,
-      ...params,
-    },
-  });
+export const fetchFilteredGuitarsList = createAsyncThunk<Promise<void>, QueryParametersType, {state: {filters: IFilters, pagination: IPagination}}>(
+  ActionType.FETCH_FILTERED_GUITARS,
+  async (params: QueryParametersType, thunkApi) => {
+    const response = await axiosInstance.get(`${process.env.REACT_APP_SERVER_URL}${ApiRoute.Guitars}`, {
+      params: {
+        [QueryParam.StringCount]: thunkApi.getState().filters.quantityOfStrings,
+        [QueryParam.Type]: thunkApi.getState().filters.guitarType,
+        [QueryParam.PriceGte]: thunkApi.getState().filters.priceRange.min || null,
+        [QueryParam.PriceLte]: thunkApi.getState().filters.priceRange.max || null,
+        [QueryParam.Limit]: GUITARS_PER_PAGE,
+        [QueryParam.Start]: GUITARS_PER_PAGE * (thunkApi.getState().pagination.currentPage - 1),
+        [QueryParam.Embed]: EmbedComments,
+        ...params,
+      },
+    });
 
-  const allPages = Math.ceil(response.headers[TotalCountHeader] / GUITARS_PER_PAGE);
-  dispatch(loadTotalPageCount(allPages));
-  dispatch(loadFilteredGuitars(response.data));
-};
 
-export const fetchGuitarsListByName = (name: string): ThunkAction<Promise<void>, RootState, AxiosInstance, Action> => async (dispatch, _getState, api) => {
-  const response = await api.get(`${process.env.REACT_APP_SERVER_URL}${ApiRoute.Guitars}`, {
-    params: {
-      [QueryParam.NameLike]: name,
-    },
-  });
+    const allPages = Math.ceil(response.headers[TotalCountHeader] / GUITARS_PER_PAGE);
+    thunkApi.dispatch(loadTotalPageCount(allPages));
+    thunkApi.dispatch(loadFilteredGuitars(response.data));
+  },
+);
 
-  dispatch(loadGuitarsByName(response.data));
-};
+export const fetchGuitarsListByName = createAsyncThunk(
+  ActionType.FETCH_GUITARS_BY_NAME,
+  async (name: string, thunkAPI) => {
+    const response = await axiosInstance.get(`${process.env.REACT_APP_SERVER_URL}${ApiRoute.Guitars}`, {
+      params: {
+        [QueryParam.NameLike]: name,
+      },
+    });
+
+    thunkAPI.dispatch(loadGuitarsByName(response.data));
+  },
+);
