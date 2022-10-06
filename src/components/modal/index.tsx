865cc592aec458ae900b7ab721a0e1ce09c273a1
Andrei Druzhaev <andrewdruzhaev@gmail.com>
2022-10-06 13:22:53 +0400

    Implement common Modal component

---
+++ src/components/modal/index.tsx
@@ -0,0 +1,21 @@
+import cn from 'classnames';
+import { ReactNode, ForwardedRef, forwardRef } from 'react';
+
+export const Modal = forwardRef((props: {
+  className?: string
+  children: ReactNode
+  isModalShown: boolean
+}, ref: ForwardedRef<HTMLDivElement>) => (
+  <div>
+    <div className={cn('modal', props.isModalShown ? 'is-active' : '', props.className)}>
+      <div className="modal__wrapper">
+        <div className="modal__overlay" data-close-modal></div>
+        <div className="modal__content" ref={ref}>
+          {props.children}
+        </div>
+      </div>
+    </div>
+  </div>
+));
+
+Modal.displayName = 'Modal';
