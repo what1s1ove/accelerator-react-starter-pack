1684642a115d32d9cd419a0ee885177ca1cbffa5
Andrei Druzhaev <andrewdruzhaev@gmail.com>
2022-10-07 13:01:26 +0400

    Add react-focus-lock package & use it

--- src/components/add-review-modal/index.tsx
+++ src/components/add-review-modal/index.tsx
@@ -5,6 +5,7 @@ import { useForm } from '../../hooks/use-form';
 import { ICommentPost } from '../../types/IComment';
 import { useDispatch } from 'react-redux';
 import { sendReviewToGuitar } from '../../store/guitars/slice';
+import LockFocus from 'react-focus-lock';
 
 export function AddReviewModal(props: {
   handleModalClose: () => void
@@ -29,101 +30,103 @@ export function AddReviewModal(props: {
   useOnClickOutside(addReviewModalRef, props.handleModalClose);
 
   return (
-    <Modal className='modal--review' isModalShown={props.isModalShown} ref={addReviewModalRef}>
-      <h2 className="modal__header modal__header--review title title--medium">Оставить отзыв</h2>
-      <h3 className="modal__product-name title title--medium-20 title--uppercase">{props.guitarName}</h3>
+    <LockFocus>
+      <Modal className='modal--review' isModalShown={props.isModalShown} ref={addReviewModalRef}>
+        <h2 className="modal__header modal__header--review title title--medium">Оставить отзыв</h2>
+        <h3 className="modal__product-name title title--medium-20 title--uppercase">{props.guitarName}</h3>
 
-      <form className="form-review" onSubmit={handleFormSubmit}>
-        <div className="form-review__wrapper">
-          <div className="form-review__name-wrapper">
-            <label className="form-review__label form-review__label--required" htmlFor="user-name">
+        <form className="form-review" onSubmit={handleFormSubmit}>
+          <div className="form-review__wrapper">
+            <div className="form-review__name-wrapper">
+              <label className="form-review__label form-review__label--required" htmlFor="user-name">
               Ваше Имя
-            </label>
-            <input
-              className="form-review__input form-review__input--name"
-              onChange={handleFormChange}
-              value={values.userName || ''}
-              name="userName"
-              id="user-name"
-              type="text"
-              autoComplete="off"
-            />
+              </label>
+              <input
+                className="form-review__input form-review__input--name"
+                onChange={handleFormChange}
+                value={values.userName || ''}
+                name="userName"
+                id="user-name"
+                type="text"
+                autoComplete="off"
+              />
 
-            {errors.userName && <span className="form-review__warning">{errors.userName}</span>}
-          </div>
-          <div>
-            <span className="form-review__label form-review__label--required">
+              {errors.userName && <span className="form-review__warning">{errors.userName}</span>}
+            </div>
+            <div>
+              <span className="form-review__label form-review__label--required">
               Ваша Оценка
-            </span>
-            <div className="rate rate--reverse">
-              <input className="visually-hidden" onChange={handleFormChange} type="radio" id="star-5" name="rating" value="5" />
-              <label className="rate__label" htmlFor="star-5" title="Отлично" />
-              <input className="visually-hidden" onChange={handleFormChange} type="radio" id="star-4" name="rating" value="4" />
-              <label className="rate__label" htmlFor="star-4" title="Хорошо" />
-              <input className="visually-hidden" onChange={handleFormChange} type="radio" id="star-3" name="rating" value="3" />
-              <label className="rate__label" htmlFor="star-3" title="Нормально" />
-              <input className="visually-hidden" onChange={handleFormChange} type="radio" id="star-2" name="rating" value="2" />
-              <label className="rate__label" htmlFor="star-2" title="Плохо" />
-              <input className="visually-hidden" onChange={handleFormChange} type="radio" id="star-1" name="rating" value="1" />
-              <label className="rate__label" htmlFor="star-1" title="Ужасно" />
-              <span className="rate__count" />
+              </span>
+              <div className="rate rate--reverse">
+                <input className="visually-hidden" onChange={handleFormChange} type="radio" id="star-5" name="rating" value="5" />
+                <label className="rate__label" htmlFor="star-5" title="Отлично" />
+                <input className="visually-hidden" onChange={handleFormChange} type="radio" id="star-4" name="rating" value="4" />
+                <label className="rate__label" htmlFor="star-4" title="Хорошо" />
+                <input className="visually-hidden" onChange={handleFormChange} type="radio" id="star-3" name="rating" value="3" />
+                <label className="rate__label" htmlFor="star-3" title="Нормально" />
+                <input className="visually-hidden" onChange={handleFormChange} type="radio" id="star-2" name="rating" value="2" />
+                <label className="rate__label" htmlFor="star-2" title="Плохо" />
+                <input className="visually-hidden" onChange={handleFormChange} type="radio" id="star-1" name="rating" value="1" />
+                <label className="rate__label" htmlFor="star-1" title="Ужасно" />
+                <span className="rate__count" />
 
-              {errors.rating && <span className="rate__message">{errors.rating}</span>}
+                {errors.rating && <span className="rate__message">{errors.rating}</span>}
+              </div>
             </div>
           </div>
-        </div>
 
-        <label className="form-review__label" htmlFor="pros">Достоинства</label>
-        <input
-          className="form-review__input"
-          onChange={handleFormChange}
-          value={values.advantage || ''}
-          name="advantage"
-          id="pros"
-          type="text"
-          autoComplete="off"
-        />
+          <label className="form-review__label" htmlFor="pros">Достоинства</label>
+          <input
+            className="form-review__input"
+            onChange={handleFormChange}
+            value={values.advantage || ''}
+            name="advantage"
+            id="pros"
+            type="text"
+            autoComplete="off"
+          />
 
-        <label className="form-review__label" htmlFor="cons">Недостатки</label>
-        <input
-          className="form-review__input"
-          onChange={handleFormChange}
-          value={values.disadvantage || ''}
-          name="disadvantage"
-          id="cons"
-          type="text"
-          autoComplete="off"
-        />
+          <label className="form-review__label" htmlFor="cons">Недостатки</label>
+          <input
+            className="form-review__input"
+            onChange={handleFormChange}
+            value={values.disadvantage || ''}
+            name="disadvantage"
+            id="cons"
+            type="text"
+            autoComplete="off"
+          />
 
-        <label className="form-review__label" htmlFor="comment">Комментарий</label>
-        <textarea
-          className="form-review__input form-review__input--textarea"
-          onChange={handleFormChange}
-          value={values.comment || ''}
-          name="comment"
-          id="comment"
-          rows={10}
-        />
+          <label className="form-review__label" htmlFor="comment">Комментарий</label>
+          <textarea
+            className="form-review__input form-review__input--textarea"
+            onChange={handleFormChange}
+            value={values.comment || ''}
+            name="comment"
+            id="comment"
+            rows={10}
+          />
+
+          <button
+            className="button button--medium-20 form-review__button"
+            type="submit"
+          >
+          Отправить отзыв
+          </button>
+        </form>
 
         <button
-          className="button button--medium-20 form-review__button"
-          type="submit"
+          className="modal__close-btn button-cross"
+          onClick={props.handleModalClose}
+          type="button"
+          aria-label="Закрыть"
+          data-testid="close-review"
         >
-          Отправить отзыв
+          <span className="button-cross__icon" />
+          <span className="modal__close-btn-interactive-area" />
         </button>
-      </form>
-
-      <button
-        className="modal__close-btn button-cross"
-        onClick={props.handleModalClose}
-        type="button"
-        aria-label="Закрыть"
-        data-testid="close-review"
-      >
-        <span className="button-cross__icon" />
-        <span className="modal__close-btn-interactive-area" />
-      </button>
-    </Modal>
+      </Modal>
+    </LockFocus>
   );
 }
 
--- src/components/success-review-modal/index.tsx
+++ src/components/success-review-modal/index.tsx
@@ -3,6 +3,7 @@ import { Modal } from '../modal';
 import sprite from '../../assets/sprite.svg';
 import { useOnClickOutside } from '../../hooks/use-outside-click';
 import { useRef } from 'react';
+import LockFocus from 'react-focus-lock';
 
 export function SuccessReviewModal(props: {
   handleModalClose: () => void
@@ -12,26 +13,28 @@ export function SuccessReviewModal(props: {
   useOnClickOutside(successReviewModalRef, props.handleModalClose);
 
   return (
-    <Modal className="modal--success" isModalShown={props.isModalShown} ref={successReviewModalRef}>
-      <Icon className="modal__icon" width="26" height="20" name={`${sprite}#icon-success`} />
-      <p className="modal__message">Спасибо за ваш отзыв!</p>
-      <div className="modal__button-container modal__button-container--review">
-        <button
-          className="button button--small modal__button modal__button--review"
-          onClick={props.handleModalClose}
-        >
+    <LockFocus>
+      <Modal className="modal--success" isModalShown={props.isModalShown} ref={successReviewModalRef}>
+        <Icon className="modal__icon" width="26" height="20" name={`${sprite}#icon-success`} />
+        <p className="modal__message">Спасибо за ваш отзыв!</p>
+        <div className="modal__button-container modal__button-container--review">
+          <button
+            className="button button--small modal__button modal__button--review"
+            onClick={props.handleModalClose}
+          >
             К покупкам!
+          </button>
+        </div>
+        <button
+          className="modal__close-btn button-cross"
+          onClick={props.handleModalClose}
+          type="button"
+          aria-label="Закрыть"
+        >
+          <span className="button-cross__icon" />
+          <span className="modal__close-btn-interactive-area" />
         </button>
-      </div>
-      <button
-        className="modal__close-btn button-cross"
-        onClick={props.handleModalClose}
-        type="button"
-        aria-label="Закрыть"
-      >
-        <span className="button-cross__icon" />
-        <span className="modal__close-btn-interactive-area" />
-      </button>
-    </Modal>
+      </Modal>
+    </LockFocus>
   );
 }
